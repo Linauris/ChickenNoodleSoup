f767aaf33f1276193bbd26158c0ba4cc2cdc5d23
Linauris <linauris.crescioni@gmail.com>
2025-03-08 15:35:33 -0500

    Initial Commit

---
+++ ChickenNoodleSoup/ChickenNoodleSoupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenNoodleSoupApp.swift
+//  ChickenNoodleSoup
+//
+//  Created by Linauris Crescioni on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ChickenNoodleSoupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickenNoodleSoupTests/ChickenNoodleSoupTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenNoodleSoupTests.swift
+//  ChickenNoodleSoupTests
+//
+//  Created by Linauris Crescioni on 3/8/25.
+//
+
+import Testing
+@testable import ChickenNoodleSoup
+
+struct ChickenNoodleSoupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
